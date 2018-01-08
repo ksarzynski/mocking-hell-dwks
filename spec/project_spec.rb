a105b31a4e3002998c70865b12fd46684d57d42c
ksarzynski <kamil.s.sarzynski@gmail.com>
2018-01-09 00:08:57 +0100

    fix test

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Project' do
   context '#new' do
     let(:id) { 1 }
     let(:name) { 'projekt' }
-    let(:start_date) { Date.parse('2018-01-05 00:00:00')  }
-    let(:deadline_date) { Date.parse('2018-01-05 00:00:00')  }
+    let(:start_date) { DateTime.parse('2018-01-05 00:00:00')  }
+    let(:deadline_date) { DateTime.parse('2018-01-05 00:00:00')  }
     subject(:project) { Project.new id, name, start_date, deadline_date}
 
     it 'creates a new project' do
@@ -23,17 +23,17 @@ RSpec.describe 'Project' do
 
     it 'returns correct date' do
       expect(project.start_date.to_s).to eq(start_date.to_s)
-      expect(project.start_date).to be_a(Date)
+      expect(project.start_date).to be_a(DateTime)
     end
 
     it 'returns correct date' do
       expect(project.deadline_date.to_s).to eq(deadline_date.to_s)
-      expect(project.deadline_date).to be_a(Date)
+      expect(project.deadline_date).to be_a(DateTime)
     end
   end
 
   context '#to_s' do
-    subject(:project) { Project.new 1, 'project1', Date.parse('2018-01-05 00:00:00'), Date.parse('2018-01-05 00:00:00') }
+    subject(:project) { Project.new 1, 'project1', DateTime.parse('2018-01-05 00:00:00'), DateTime.parse('2018-01-05 00:00:00') }
 
     it 'returns correct output' do
       expect(project.to_s).to be_a(String).and include('project1 - 2018-01-05 00:00:00 - 2018-01-05 00:00:00')
