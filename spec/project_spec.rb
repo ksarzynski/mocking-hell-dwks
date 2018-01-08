74dc27d1ccec6ce826d777b83fd851586c1328ef
ksarzynski <kamil.s.sarzynski@gmail.com>
2018-01-09 00:22:05 +0100

    fix test

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Project' do
   context '#new' do
     let(:id) { 1 }
     let(:name) { 'projekt' }
-    let(:start_date) { DateTime.parse('2018-01-05 00:00:00')  }
-    let(:deadline_date) { DateTime.parse('2018-01-05 00:00:00')  }
-    subject(:project) { Project.new id, name, start_date, deadline_date}
+    let(:date) { Date.parse('2018-01-09')  }
+    let(:deadlinedate) { Date.parse('2018-01-09')  }
+    subject(:project) { Project.new id, name, date, deadlinedate}
 
     it 'creates a new project' do
       expect { project }.not_to raise_error
@@ -22,21 +22,21 @@ RSpec.describe 'Project' do
     end
 
     it 'returns correct date' do
-      expect(project.start_date.to_s).to eq(start_date.to_s)
-      expect(project.start_date).to be_a(DateTime)
+      expect(project.date.to_s).to eq(date.to_s)
+      expect(project.date).to be_a(Date)
     end
 
     it 'returns correct date' do
-      expect(project.deadline_date.to_s).to eq(deadline_date.to_s)
-      expect(project.deadline_date).to be_a(DateTime)
+      expect(project.deadlinedate.to_s).to eq(deadlinedate.to_s)
+      expect(project.deadlinedate).to be_a(Date)
     end
   end
 
   context '#to_s' do
-    subject(:project) { Project.new 1, 'project1', DateTime.parse('2018-01-05 00:00:00'), DateTime.parse('2018-01-05 00:00:00') }
+    subject(:project) { Project.new 1, 'project1', Date.parse('2018-01-05'), Date.parse('2018-01-05') }
 
     it 'returns correct output' do
-      expect(project.to_s).to be_a(String).and include('project1 - 2018-01-05 00:00:00 - 2018-01-05 00:00:00')
+      expect(project.to_s).to be_a(String).and include('project1 - 2018-01-05 - 2018-01-05')
     end
   end
 end
